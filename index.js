7cc01ee41f83a02b4ce89239a2af65add3d3b81f
HassanObeid89 <61018141+HassanObeid89@users.noreply.github.com>
2021-04-13 12:16:46 +0200

    Added two modules to the database (user & post) then fetched the data.

--- index.js
+++ index.js
@@ -1,27 +1,44 @@
+//Dependecy imports
 const { ApolloServer } = require('apollo-server')
 const gql = require('graphql-tag')
 const mongoose = require('mongoose');
 
+//Relative imports
+const Post = require('./modules/Post');
 const { MONGODB } = require('./config.js');
 
 const typeDefs = gql`
-    type Query{
-        sayHi: String!
+    type Post{
+        id: ID!
+        body: String!
+        createdAt: String!
+        username: String!
     }
-`
+    type Query{
+        getPosts: [Post]
+    }
+`;
 
 const resolvers = {
     Query: {
-        sayHi: () => 'Hello World!'
+        async getPosts() {
+            try{
+                const Posts = await Post.find();
+                return Posts;
+            }
+            catch (err){
+            throw new Error(err);
+            }
+        }
     }
-}
+};
 
 const server = new ApolloServer({
     typeDefs,
     resolvers
 });
 
-mongoose.connect(MONGODB, {useNewUrlParser: true}, {useUnifiedTopology: true})
+mongoose.connect(MONGODB, {useNewUrlParser: true})
     .then(() => {
         console.log('MongoDB connected' );
         return server.listen({ port: 5000 });
