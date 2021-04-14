6d751c8e407f218ee40d97dbec28d4bce057c147
HassanObeid89 <61018141+HassanObeid89@users.noreply.github.com>
2021-04-14 12:35:44 +0200

    Added the functionality to validate the login user input.

--- util/validators.js
+++ util/validators.js
@@ -24,3 +24,17 @@ module.exports.validateRegisterInput = (
         valid: Object.keys(errors).length < 1
     };
 };
+
+module.exports.validateLoginInput = (username, password) => {
+    const errors = {}; 
+    if (username.trim() == '') {
+        errors.email = 'Email must not be empty';
+    }
+    if (password.trim() == '') {
+        errors.email = 'Password must not be empty';
+    }
+    return {
+        errors,
+        valid: Object.keys(errors).length < 1
+    };
+};
